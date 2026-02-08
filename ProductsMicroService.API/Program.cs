90d69f4da3a8834e4ed2d96a57d2ce6893f74356
Amit <amitpstu1@gmail.com>
2026-02-08 22:22:12 +0600

    first commit

---
+++ ProductsMicroService.API/Program.cs
@@ -0,0 +1,29 @@
+
+
+using BusinessLogicLayer;
+using DataAccessLayer;
+using FluentValidation.AspNetCore;
+using ProductsMicroService.API.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add DAL services and BLL services to the container.
+builder.Services.AddDataAccessLayer();
+builder.Services.AddBusinessLogicLayer();
+
+builder.Services.AddControllers();
+
+// FluentValidation
+builder.Services.AddFluentValidationAutoValidation();
+
+var app = builder.Build();
+
+app.UseExceptionHandlingMiddleware();
+app.UseRouting();
+
+// Auth
+app.UseAuthorization();
+app.UseAuthentication();
+
+app.MapControllers();
+app.Run();
