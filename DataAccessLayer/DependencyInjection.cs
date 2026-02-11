5804d5c639fcbb18c8ad5ce885e93678ecccb600
Amit <amitpstu1@gmail.com>
2026-02-11 15:00:37 +0600

    Added Repo

--- DataAccessLayer/DependencyInjection.cs
+++ DataAccessLayer/DependencyInjection.cs
@@ -14,7 +14,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddDataAccessLayer(this IServiceCollection services,
         IConfiguration configuration)
-    {
+    { 
         services.AddDbContext<ApplicationDbContext>(options =>
         {
             options.UseMySQL(configuration.GetConnectionString("DefaultConnection")!);
